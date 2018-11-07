0114cbd737565fe23eec7f82d4426f080bffb9c0
VisnjaDjukic <visnjadj@hotmail.com>
2018-11-07 08:01:22 +0100

    mario

--- DOM/VI WEEK/2_day - Events/Propagation/propagation.js
+++ DOM/VI WEEK/2_day - Events/Propagation/propagation.js
@@ -1,9 +1,24 @@
 $image = document.querySelectorAll('img');
+$body = document.querySelector('body');
 for (var i = 0; i < $image.length; i++) {
+
     $image[i].addEventListener('click', addBorder);
+
 }
 
 function addBorder(event) {
-
+    if (event.target.width !=300) {
     event.target.style.border = '5px solid red';
+} else {
+    event.target.style.border = '5px solid red';
+    event.stopPropagation();
 }
+}
+
+document.addEventListener('click', addPrint);
+function addPrint(event) {
+    console.log(event.target.alt);
+}
+
+
+
