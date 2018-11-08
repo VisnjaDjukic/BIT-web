4e35f207f6eeac7d99fc3cc7f4bf456fc141e377
VisnjaDjukic <visnjadj@hotmail.com>
2018-11-09 00:05:26 +0100

    mario-homework

--- DOM/VI WEEK/2_day - Events_MovieForm/Mario/runningMario.js
+++ DOM/VI WEEK/2_day - Events_MovieForm/Mario/runningMario.js
@@ -6,11 +6,14 @@ $runningMario = document.querySelector('.runningMario')
 $body.addEventListener('keydown', runningMario);
 $body.addEventListener('keyup', stopRunningMario);
 
+var x = 0;
 function runningMario(event) {
 
     if (event.keyCode === 39) {
+        x += 100;
         $mario.style.display = 'none';
         $runningMario.style.display = 'inline-block';
+        $body.style.backgroundPosition = x+'px';
 
     }
 }
